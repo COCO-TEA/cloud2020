3d1f0c95fa6aa9e84104ac60be509a2db2eee263
September <954670268@qq.com>
2020-08-06 18:02:31 +0800

    add

--- cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/PaymentMain9001.java
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/PaymentMain9001.java
@@ -2,13 +2,14 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableHystrixDashboard
-public class HystrixDashboardMain9001 {
+@EnableDiscoveryClient
+public class PaymentMain9001 {
 
     public static void main(String[] args) {
-        SpringApplication.run(HystrixDashboardMain9001.class, args);
+        SpringApplication.run(PaymentMain9001.class, args);
     }
+
 }
